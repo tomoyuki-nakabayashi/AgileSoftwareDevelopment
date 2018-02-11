c0df63b18b7bc4ffece38ecfa8204ca1d975bc51
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-02-11 21:03:40 +0900

    chore: Preparing environment.

---
+++ 01.SaralySystem/test/test_add_salaried_employee.cpp
@@ -0,0 +1,13 @@
+// Copyright <2018> Tomoyuki-Nakabayashi
+// This software is released under the MIT License, see LICENSE.
+
+#include <gtest/gtest.h>
+
+namespace add_employee_transaction_test {
+class AddSalariedEmployeeTest : public ::testing::Test {
+};
+
+TEST_F(AddSalariedEmployeeTest, FirstTest) {
+  EXPECT_TRUE(true);
+}
+}  // namespace add_salaried_employee_test
