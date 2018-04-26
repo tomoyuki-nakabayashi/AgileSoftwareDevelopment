cb20ba37c045babb66589e2141d8567cdc3ceca4
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-04-26 20:32:57 +0900

    Add test.

---
+++ SerialDate/tests/org/jfree/date/SerialDate/SerialDateTest.java
@@ -0,0 +1,17 @@
+package org.jfree.date.SerialDate;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SerialDateTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
