da456056b44ac72d6b826318ff9cfc15cd8d71af
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-05-13 16:01:23 +0900

    case study: SerialDate Finish to copy BobsTest.

--- SerialDate/jcommon/src/test/java/org/jfree/date/BobsSerialDateTest.java
+++ SerialDate/jcommon/src/test/java/org/jfree/date/BobsSerialDateTest.java
@@ -270,10 +270,48 @@ public class BobsSerialDateTest extends TestCase {
         assertEquals(d(31, OCTOBER, 2006), d.getEndOfCurrentMonth(d(1, OCTOBER, 2006)));
         assertEquals(d(30, NOVEMBER, 2006), d.getEndOfCurrentMonth(d(1, NOVEMBER, 2006)));
         assertEquals(d(31, DECEMBER, 2006), d.getEndOfCurrentMonth(d(1, DECEMBER, 2006)));
-        assertEquals(d(29, , 2008), d.getEndOfCurrentMonth(d(1, FEBRUARY, 2006)));
+        assertEquals(d(29, FEBRUARY, 2008), d.getEndOfCurrentMonth(d(1, FEBRUARY, 2008)));
     }
 
     public void testWeekInMonthToString() throws Exception {
-        assertEquals("First", week);
+        assertEquals("First", weekInMonthToString(FIRST_WEEK_IN_MONTH));
+        assertEquals("Second", weekInMonthToString(SECOND_WEEK_IN_MONTH));
+        assertEquals("Third", weekInMonthToString(THIRD_WEEK_IN_MONTH));
+        assertEquals("Fourth", weekInMonthToString(FOURTH_WEEK_IN_MONTH));
+        assertEquals("Last", weekInMonthToString(LAST_WEEK_IN_MONTH));
+
+        //try {
+        //    weekInMonthToString(-1);
+        //    fail("Invalid week code should throw exception");
+        //} catch (IllegalArgumentException e) {}
+    }
+
+    public void testRelativeToString() throws Exception {
+        assertEquals("Preceding", relativeToString(PRECEDING));
+        assertEquals("Nearest", relativeToString(NEAREST));
+        assertEquals("Following", relativeToString(FOLLOWING));
+
+        //try {
+        //    relativeToString(-1000);
+        //    fail("Invalid relative code should throw exception");
+        //} catch (IllegalArgumentException e) {}
+    }
+
+    public void testCreateInstanceFromDDMMYYYY() throws Exception {
+        SerialDate date = createInstance(1, JANUARY, 1900);
+        assertEquals(1, date.getDayOfMonth());
+        assertEquals(JANUARY, date.getMonth());
+        assertEquals(1900, date.getYYYY());
+        assertEquals(2, date.toSerial());
+    }
+
+    public void testCreateInstanceFromSerial() throws Exception {
+        assertEquals(d(1, JANUARY, 1900), createInstance(2));
+        assertEquals(d(1, JANUARY, 1901), createInstance(367));
+    }
+
+    public void testCreateInstanceFromJavaDate() throws Exception {
+        assertEquals(d(1, JANUARY, 1900), createInstance((new GregorianCalendar(1900, 0, 1).getTime())));
+        assertEquals(d(1, JANUARY, 2006), createInstance(new GregorianCalendar(2006, 0 , 1).getTime()));
     }
 }
