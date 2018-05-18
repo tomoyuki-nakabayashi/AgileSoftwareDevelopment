b162a24b2c12bdaeeed4f1b618e92d33e759318c
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-05-18 20:56:28 +0900

    case study: SerialDate pp.355

--- SerialDate/jcommon/src/main/java/org/jfree/date/SpreadsheetDate.java
+++ SerialDate/jcommon/src/main/java/org/jfree/date/SpreadsheetDate.java
@@ -83,8 +83,14 @@ public class SpreadsheetDate extends DayDate {
 
     /** For serialization. */
     private static final long serialVersionUID = -2039586705374454461L;
-    
-    /** 
+
+    /** The serial number for 1 January 1900. */
+    public static final int SERIAL_LOWER_BOUND = 2;
+
+    /** The serial number for 31 December 9999. */
+    public static final int SERIAL_UPPER_BOUND = 2958465;
+
+    /**
      * The day number (1-Jan-1900 = 2, 2-Jan-1900 = 3, ..., 31-Dec-9999 = 
      * 2958465). 
      */
